fb1f64afbf8dc3b772df622469cd387fd4379b03
Vikram Barr <vikram@netrino.com>
2022-07-18 15:03:46 -0400

    Adds images to tabs, waiting to work on my mac so i can yoink the showdown type colors.

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import pygame
 from threading import Thread
 import time
 import requests
+import shutil
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -18,17 +19,38 @@ topgap = 5
 bottomgap = 150
 bordersize = 5
 tabsize = ((winsize[0]-(2*bordersize))/6)
+tabheight = 40
 
-mons = ["Zacian-Crowned", "Ditto", "Eternatus", "Pyukumuku", "Blissey", "Sableye-Mega"]
+listofmons = ["Ampharos", "Camerupt-Mega", "Ferroseed", "Pyukumuku", "Blissey", "Charizard-Mega-X"]
 
+def scrapeimages(mons):
+    sesh = requests.session()
+    for i, mon in enumerate(mons):
+        info = sesh.get("https://pokeapi.co/api/v2/pokemon/" + mon.lower()).json()
+        image = sesh.get(info["sprites"]["front_default"], stream=True)
+        with open(f"images\\{i}.png", "wb") as of:
+            shutil.copyfileobj(image.raw, of)
 
+scrapeimages(listofmons)  
 
+images = []
+tabcolors = []
 
+for i, mon in enumerate(listofmons):
+    images.append(pygame.image.load(f"images/{i}.png"))
+
+for image in images:
+    if pygame.transform.average_color(image) != (0, 0, 0, 0):
+        tabcolors.append(pygame.transform.average_color(image))
+    else:
+        tabcolors.append(pygame.transform.average_color(image, rect=pygame.Rect(43, 43, 10, 10)))
+print(tabcolors)
+    
 def drawtab(tab):
     screen.fill(border)
-    screen.fill(unselected, rect=pygame.Rect(bordersize, topgap, winsize[0]-(2*bordersize), 30))
-    screen.fill(selected, rect=pygame.Rect(bordersize + (tab*tabsize), topgap, tabsize, 30))
-    screen.fill(selected, rect=pygame.Rect(bordersize, topgap+30, winsize[0]-(2*bordersize), winsize[1]-30-topgap-bottomgap))
+    screen.fill(unselected, rect=pygame.Rect(bordersize, topgap, winsize[0]-(2*bordersize), tabheight))
+    screen.fill(selected, rect=pygame.Rect(bordersize + (tab*tabsize), topgap, tabsize, tabheight))
+    screen.fill(selected, rect=pygame.Rect(bordersize, topgap+tabheight, winsize[0]-(2*bordersize), winsize[1]-tabheight-topgap-bottomgap))
     screen.fill(unselected, rect=pygame.Rect(bordersize, winsize[1]-bottomgap+bordersize, winsize[0]-(2*bordersize), bottomgap-(bordersize*2)))
 
 pygame.display.flip()
@@ -38,21 +60,30 @@ print(pygame.display.Info())
 tab = 0
 hovertab = None
 running = True
+
 while running:
+    
     for event in pygame.event.get():
+        
         if event.type == pygame.QUIT:
             running = False
-        if event.type == 1025 and event.dict["button"] == 1 and topgap < event.dict["pos"][1] < topgap+30:
+            
+        if event.type == 1025 and event.dict["button"] == 1 and topgap < event.dict["pos"][1] < topgap+tabheight:
             tab = int((event.dict["pos"][0]-bordersize)/tabsize)
+            
         if event.type == 1024:
-            if topgap < event.dict["pos"][1] < (topgap+30):
+            if topgap < event.dict["pos"][1] < (topgap+tabheight):
                 hovertab = int((event.dict["pos"][0]-bordersize)/tabsize)
             else:
                 hovertab = None
             
     drawtab(tab)
+    
     if hovertab != tab and hovertab != None and hovertab <= 5:
-        screen.fill(hover, rect=pygame.Rect(bordersize + (hovertab*tabsize), topgap, tabsize, 30))
+        screen.fill(hover, rect=pygame.Rect(bordersize + (hovertab*tabsize), topgap, tabsize, tabheight))
+        
+    for i, image in enumerate(images):
+        screen.blit(pygame.transform.scale(image, (50, 50)), (bordersize + (tabsize*i) + 5, topgap - 5))
         
     pygame.display.flip()
     clock.tick(30)
